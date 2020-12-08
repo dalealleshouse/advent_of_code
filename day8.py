1f1ae57c1dac0478f73b8e0ba552bc1113d7608e
Dale Alleshouse <dalealleshouse@gmail.com>
2020-12-08 11:59:50 -0500

    cleanup

--- day8.py
+++ day8.py
@@ -2,15 +2,15 @@ import re
 import linecache
 import collections
 
-Enviornment = collections.namedtuple('Enviornment', ['pc', 'acc'])
+Enviornment = collections.namedtuple('Enviornment', ['ip', 'acc'])
 Instruction = collections.namedtuple('Instruction', ['op', 'arg'])
 
-INSTRUCTION_PARSER = re.compile('(?P<inst>[\w]{3}) (?P<value>[+,-][\d]+)')
+INSTRUCTION_PARSER = re.compile('(?P<op>[\w]{3}) (?P<arg>[+,-][\d]+)')
 
 INSTRCUTIONS = {
-    'nop': lambda arg, env: Enviornment(env.pc + 1, env.acc),
-    'jmp': lambda arg, env: Enviornment(env.pc + arg, env.acc),
-    'acc': lambda arg, env: Enviornment(env.pc + 1, env.acc + arg),
+    'nop': lambda arg, env: Enviornment(env.ip + 1, env.acc),
+    'jmp': lambda arg, env: Enviornment(env.ip + arg, env.acc),
+    'acc': lambda arg, env: Enviornment(env.ip + 1, env.acc + arg),
     'term': lambda arg, env: Enviornment(-1, env.acc)
 }
 
@@ -21,11 +21,7 @@ def get_inst(instructions, line_no):
         return Instruction('term', -1)
 
     match = INSTRUCTION_PARSER.search(raw)
-    return Instruction(match.group('inst'), int(match.group('value')))
-
-
-def execute_instruction(inst, env):
-    return INSTRCUTIONS[inst.op](inst.arg, env)
+    return Instruction(match.group('op'), int(match.group('arg')))
 
 
 def print_result(result):
@@ -39,17 +35,21 @@ def execute(instructions, instruction_getter=get_inst):
     loop_detector = {}
 
     env = Enviornment(1, 0)
-    while env.pc != -1:
-        loop_detector[env.pc] = True
-        inst = instruction_getter(instructions, env.pc)
-        env = execute_instruction(inst, env)
-        if env.pc in loop_detector:
+    while env.ip != -1:
+        loop_detector[env.ip] = True
+        inst = instruction_getter(instructions, env.ip)
+        env = INSTRCUTIONS[inst.op](inst.arg, env)
+        if env.ip in loop_detector:
             return (-1, env)
 
     return (0, env)
 
 
 def find_bad_instruction(instructions):
+    """
+    I opted for brute force rather than trying to solve the halting problem.
+    However, there are much more efficent solutions...
+    """
     mod_line = 1
 
     def get_mod_inst(instructions, line_no):
