d6413ad483744a3af9c619cb4cbcde610d656562
Dale Alleshouse <dalealleshouse@gmail.com>
2020-12-11 09:20:31 -0500

    clean up

--- 2020/day11.py
+++ 2020/day11.py
@@ -4,10 +4,12 @@ from copy import deepcopy
 FLOOR = '.'
 EMPTY_SEAT = 'L'
 OCCUPIED_SEAT = '#'
+DIRECTIONS = [(-1, -1), (-1, 0), (-1, 1), (0, - 1),
+              (0, 1), (1, - 1), (1, 0), (1, 1)]
 
 SeatLayout = namedtuple('SeatLayout',
-                        ['row_n', 'column_n', 'layout', 'change_count'])
-Seat = namedtuple('Seat', ['row', 'column'])
+                        ['row_n', 'col_n', 'layout', 'change_count'])
+Seat = namedtuple('Seat', ['row', 'col'])
 
 
 def occupied_count(seat_layout):
@@ -17,56 +19,43 @@ def occupied_count(seat_layout):
 def simple_adjacency_checker_factory(seat_layout):
     def occupied(seat):
         if ((not 0 <= seat.row < seat_layout.row_n)
-                or (not 0 <= seat.column < seat_layout.column_n)):
+                or (not 0 <= seat.col < seat_layout.col_n)):
             return 0
 
-        if seat_layout.layout[seat.row][seat.column] == OCCUPIED_SEAT:
+        if seat_layout.layout[seat.row][seat.col] == OCCUPIED_SEAT:
             return 1
 
         return 0
 
     def adjacency_checker(seat):
-        return (
-            occupied(Seat(seat.row - 1, seat.column - 1))
-            + occupied(Seat(seat.row - 1, seat.column))
-            + occupied(Seat(seat.row - 1, seat.column + 1))
-            + occupied(Seat(seat.row, seat.column - 1))
-            + occupied(Seat(seat.row, seat.column + 1))
-            + occupied(Seat(seat.row + 1, seat.column - 1))
-            + occupied(Seat(seat.row + 1, seat.column))
-            + occupied(Seat(seat.row + 1, seat.column + 1)))
+        return sum(occupied(Seat(seat.row + x[0], seat.col + x[1]))
+                   for x in DIRECTIONS)
 
     return adjacency_checker
 
 
 def adjacency_checker_factory(seat_layout):
     def occupied(seat, direction):
-        next_seat = seat
+        next_seat = Seat(seat.row + direction[0], seat.col + direction[1])
 
-        while True:
-            next_seat = Seat(next_seat.row + direction[0],
-                             next_seat.column + direction[1])
+        while (0 <= next_seat.row < seat_layout.row_n
+               and 0 <= next_seat.col < seat_layout.col_n):
 
-            if ((not 0 <= next_seat.row < seat_layout.row_n)
-                    or (not 0 <= next_seat.column < seat_layout.column_n)):
-                return 0
-
-            value = seat_layout.layout[next_seat.row][next_seat.column]
-
-            if value == FLOOR:
-                continue
+            value = seat_layout.layout[next_seat.row][next_seat.col]
 
             if value == OCCUPIED_SEAT:
                 return 1
 
-            return 0
+            if value == EMPTY_SEAT:
+                return 0
+
+            next_seat = Seat(next_seat.row + direction[0],
+                             next_seat.col + direction[1])
+
+        return 0
 
     def adjacency_checker(seat):
-        return (
-            occupied(seat, (-1, -1)) + occupied(seat, (-1, 0))
-            + occupied(seat, (-1, 1)) + occupied(seat, (0, - 1))
-            + occupied(seat, (0, 1)) + occupied(seat, (1, - 1))
-            + occupied(seat, (1, 0)) + occupied(seat, (1, 1)))
+        return sum(occupied(seat, x) for x in DIRECTIONS)
 
     return adjacency_checker
 
@@ -77,21 +66,20 @@ def apply_rules(seat_layout, factory, seat_tolerance):
     change_count = 0
 
     for row in range(seat_layout.row_n):
-        for column, state in enumerate(seat_layout.layout[row]):
+        for col, state in enumerate(seat_layout.layout[row]):
             if (state == EMPTY_SEAT
-                    and adjacency_checker(Seat(row, column)) == 0):
+                    and adjacency_checker(Seat(row, col)) == 0):
                 change_count += 1
-                new_layout[row][column] = OCCUPIED_SEAT
+                new_layout[row][col] = OCCUPIED_SEAT
                 continue
 
             if (state == OCCUPIED_SEAT
-                    and adjacency_checker(Seat(row, column))
-                    >= seat_tolerance):
+                    and adjacency_checker(Seat(row, col)) >= seat_tolerance):
                 change_count += 1
-                new_layout[row][column] = EMPTY_SEAT
+                new_layout[row][col] = EMPTY_SEAT
                 continue
 
-    return SeatLayout(seat_layout.row_n, seat_layout.column_n,
+    return SeatLayout(seat_layout.row_n, seat_layout.col_n,
                       new_layout, change_count)
 
 
