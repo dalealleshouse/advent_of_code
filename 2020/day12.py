ccfec0a360a2096e9d4cc8e825bbc09d7c15e684
Dale Alleshouse <dalealleshouse@gmail.com>
2020-12-12 18:17:42 -0500

    better 2d rotation algorithm

--- 2020/day12.py
+++ 2020/day12.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from math import cos, sin, radians
 import re
 
 
@@ -10,72 +11,68 @@ class NavInst:
 
 @dataclass
 class Position:
-    deg_n: int = 0
-    deg_e: int = 0
+    deg_e: int = 0  # x
+    deg_n: int = 0  # y
     heading: str = 'E'
 
 
 @dataclass
 class Ship:
     pos: 'Position' = Position()
-    waypoint: 'Position' = Position(1, 10, 'E')
+    waypoint: 'Position' = Position(10, 1, 'E')
+
+    def update_pos(self, pos):
+        self.pos = pos
+        return self
+
+    def update_wp(self, waypoint):
+        self.waypoint = waypoint
+        return self
 
 
 NAV_INST_PARSER = re.compile(r'(?P<inst>[A-Z])(?P<arg>\d+)')
 DIRS = ['N', 'E', 'S', 'W']
 DIR_MUL = {'R': 1, 'L': -1}
 NAV_INSTS = {
-    'N': lambda pos, arg: Position(pos.deg_n + arg, pos.deg_e, pos.heading),
-    'S': lambda pos, arg: Position(pos.deg_n - arg, pos.deg_e, pos.heading),
-    'E': lambda pos, arg: Position(pos.deg_n, pos.deg_e + arg, pos.heading),
-    'W': lambda pos, arg: Position(pos.deg_n, pos.deg_e - arg, pos.heading),
-    'L': lambda pos, arg: Position(pos.deg_n, pos.deg_e,
+    'N': lambda pos, arg: Position(pos.deg_e, pos.deg_n + arg, pos.heading),
+    'S': lambda pos, arg: Position(pos.deg_e, pos.deg_n - arg, pos.heading),
+    'E': lambda pos, arg: Position(pos.deg_e + arg, pos.deg_n, pos.heading),
+    'W': lambda pos, arg: Position(pos.deg_e - arg, pos.deg_n, pos.heading),
+    'L': lambda pos, arg: Position(pos.deg_e, pos.deg_n,
                                    turn(pos.heading, arg, 'L')),
-    'R': lambda pos, arg: Position(pos.deg_n, pos.deg_e,
+    'R': lambda pos, arg: Position(pos.deg_e, pos.deg_n,
                                    turn(pos.heading, arg, 'R')),
     # pylint: disable=unnecessary-lambda
     'F': lambda pos, arg: NAV_INSTS[pos.heading](pos, arg)
 }
 WP_NAV_INSTS = {
-    'N': lambda ship, arg: update_wp(ship, NAV_INSTS['N'](ship.waypoint, arg)),
-    'S': lambda ship, arg: update_wp(ship, NAV_INSTS['S'](ship.waypoint, arg)),
-    'E': lambda ship, arg: update_wp(ship, NAV_INSTS['E'](ship.waypoint, arg)),
-    'W': lambda ship, arg: update_wp(ship, NAV_INSTS['W'](ship.waypoint, arg)),
-    'L': lambda ship, arg: update_wp(
-        ship, waypoint_turn(ship.waypoint, arg, 'L')),
-    'R': lambda ship, arg: update_wp(
-        ship, waypoint_turn(ship.waypoint, arg, 'R')),
-    'F': lambda ship, arg: update_pos(
-        ship, Position(ship.pos.deg_n + ship.waypoint.deg_n * arg,
-                       ship.pos.deg_e + ship.waypoint.deg_e * arg))
+    'N': lambda ship, arg: ship.update_wp(NAV_INSTS['N'](ship.waypoint, arg)),
+    'S': lambda ship, arg: ship.update_wp(NAV_INSTS['S'](ship.waypoint, arg)),
+    'E': lambda ship, arg: ship.update_wp(NAV_INSTS['E'](ship.waypoint, arg)),
+    'W': lambda ship, arg: ship.update_wp(NAV_INSTS['W'](ship.waypoint, arg)),
+    'L': lambda ship, arg: ship.update_wp(
+        waypoint_turn(ship.waypoint, arg, 'L')),
+    'R': lambda ship, arg: ship.update_wp(
+        waypoint_turn(ship.waypoint, arg, 'R')),
+    'F': lambda ship, arg: ship.update_pos(
+        Position(ship.pos.deg_e + ship.waypoint.deg_e * arg,
+                 ship.pos.deg_n + ship.waypoint.deg_n * arg))
 }
-
-
-def update_pos(ship, pos):
-    ship.pos = pos
-    return ship
-
-
-def update_wp(ship, waypoint):
-    ship.waypoint = waypoint
-    return ship
 
 
 def turn(heading, degrees, direction='R'):
     turn_dir = DIR_MUL[direction]
-    units = (degrees // 90) * turn_dir
+    units = degrees // 90 * turn_dir
     new_direction = (units + DIRS.index(heading)) % 4
     return DIRS[new_direction]
 
 
 def waypoint_turn(waypoint, degrees, direction='R'):
-    for _ in range(degrees // 90):
-        if direction == 'R':
-            waypoint = Position(waypoint.deg_e * -1, waypoint.deg_n)
-        else:
-            waypoint = Position(waypoint.deg_e, waypoint.deg_n * -1)
-
-    return waypoint
+    angle = radians(degrees * -1) * DIR_MUL[direction]
+    # https://academo.org/demos/rotation-about-point/
+    return Position(
+        round(waypoint.deg_e * cos(angle) - waypoint.deg_n * sin(angle)),
+        round(waypoint.deg_e * sin(angle) + waypoint.deg_n * cos(angle)))
 
 
 def parse_nav_instruction(raw):
@@ -99,11 +96,11 @@ def entry_point():
     instructions = read_file('day12_input.txt')
 
     ship = navigate(Position(), instructions, NAV_INSTS)
-    print(f'Degree Sum = {abs(ship.deg_n) + abs(ship.deg_e)}')
+    print(f'Manhattan Distance = {abs(ship.deg_n) + abs(ship.deg_e)}')
     # 2847
 
     ship = navigate(Ship(), instructions, WP_NAV_INSTS)
-    print(f'Degree Sum = {abs(ship.pos.deg_n) + abs(ship.pos.deg_e)}')
+    print(f'Manhattan Distance = {abs(ship.pos.deg_n) + abs(ship.pos.deg_e)}')
     # 29839
 
 
