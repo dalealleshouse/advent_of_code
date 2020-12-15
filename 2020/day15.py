4973df6745f06ba0c22f6e908d4792f59da2da3a
Dale Alleshouse <dalealleshouse@gmail.com>
2020-12-15 17:00:17 -0500

    clean up

--- 2020/day15.py
+++ 2020/day15.py
@@ -1,11 +1,4 @@
-def process_turn(seed_data, game_state, last_num, turn):
-    if last_num is None:
-        return {}, seed_data[0]
-
-    if turn <= len(seed_data):
-        game_state[last_num] = turn
-        return game_state, seed_data[turn - 1]
-
+def process_turn(game_state, last_num, turn):
     if last_num not in game_state:
         game_state[last_num] = turn
         return game_state, 0
@@ -17,21 +10,28 @@ def process_turn(seed_data, game_state, last_num, turn):
 
 def process_game(seed_data, turns):
     game_state = {}
-    num = None
-    for turn in range(1, turns + 1):
-        game_state, num = process_turn(seed_data, game_state, num, turn)
+    num = seed_data[-1]
+
+    for turn, seed in enumerate(seed_data[:-1]):
+        game_state[seed] = turn + 2
+
+    for turn in range(len(seed_data) + 1, turns + 1):
+        game_state, num = process_turn(game_state, num, turn)
 
     return num
 
 
 def main():
     seed_data = [9, 19, 1, 6, 0, 5, 4]
+    # seed_data = [0, 3, 6]
 
     final_num = process_game(seed_data, 2020)
     print(f'Final number spoken = {final_num}')
+    # 1522
 
     final_num = process_game(seed_data, 30000000)
     print(f'Final number spoken = {final_num}')
+    # 18234
 
 
 if __name__ == '__main__':
