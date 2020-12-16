7c7046b88d2267c25c5afc7c772c68b0a9861250
Dale Alleshouse <dalealleshouse@gmail.com>
2020-12-16 14:59:26 -0500

    day 16

---
+++ 2020/day16.py
@@ -0,0 +1,161 @@
+import re
+from math import prod
+from copy import deepcopy
+from dataclasses import dataclass, field
+
+RULE_PARSER = re.compile(r'^(?P<name>[\w\s]+): '
+                         r'(?P<low_low>[\d]+)-(?P<low_high>[\d]+) or '
+                         r'(?P<high_low>[\d]+)-(?P<high_high>[\d]+)')
+
+
+@dataclass
+class Range:
+    low: int
+    high: int
+
+
+@dataclass
+class Rule:
+    name: str
+    low_range: Range
+    high_range: Range
+
+
+@dataclass
+class Ticket:
+    values: list
+
+
+@dataclass
+class TicketData:
+    rules: list = field(default_factory=list)
+    my_ticket: Ticket = None
+    tickets: list = field(default_factory=list)
+
+
+def conforms_to_rule(rule, value):
+    if (rule.low_range.low <= value <= rule.low_range.high
+            or rule.high_range.low <= value <= rule.high_range.high):
+        return True
+
+    return False
+
+
+def find_invalid_values(ticket_data):
+    good_tickets = []
+
+    invalid = []
+    for ticket in ticket_data.tickets:
+        ticket_valid = True
+        for value in ticket.values:
+            if all(not conforms_to_rule(x, value) for x in ticket_data.rules):
+                invalid.append(value)
+                ticket_valid = False
+
+        if ticket_valid:
+            good_tickets.append(ticket)
+
+    return invalid, TicketData(ticket_data.rules,
+                               ticket_data.my_ticket, good_tickets)
+
+
+def remove_field(possiblities, name, keep_index):
+    for i, possibles in enumerate(possiblities):
+        if i == keep_index:
+            continue
+
+        if name in possibles:
+            possibles.remove(name)
+
+
+def define_fields(ticket_data):
+    fields = [x.name for x in ticket_data.rules]
+    # to start, every field is equally possible for all positions
+    possiblities = list(map(lambda x: deepcopy(fields), range(len(fields))))
+
+    for ticket in ticket_data.tickets:
+        for i, value in enumerate(ticket.values):
+            for rule in ticket_data.rules:
+                if not conforms_to_rule(rule, value):
+                    possiblities[i].remove(rule.name)
+
+    # if a position has a single possible value, remove that possiblitity from
+    # all other positions
+    while sum(len(x) for x in possiblities) != len(possiblities):
+        for i, position in enumerate(possiblities):
+            if len(position) == 1:
+                remove_field(possiblities, position[0], i)
+
+    return [x[0] for x in possiblities]
+
+
+def print_ticket(value_map, ticket):
+    departure_values = []
+
+    for i, value in enumerate(value_map):
+        if value.startswith('departure'):
+            departure_values.append(ticket.values[i])
+
+        print(f'{value}: {ticket.values[i]}')
+
+    return departure_values
+
+
+def parse_file(path):
+    data = TicketData()
+
+    def parse_rule(raw):
+        match = RULE_PARSER.search(raw)
+        rule = Rule(
+            match.group('name'),
+            Range(int(match.group('low_low')), int(match.group('low_high'))),
+            Range(int(match.group('high_low')), int(match.group('high_high'))))
+
+        data.rules.append(rule)
+
+    def parse_my_ticket(raw):
+        data.my_ticket = Ticket(list(map(int, raw.split(','))))
+
+    def parse_ticket(raw):
+        ticket = Ticket(list(map(int, raw.split(','))))
+        data.tickets.append(ticket)
+
+    modes = {'rules': parse_rule,
+             'my_ticket': parse_my_ticket,
+             'ticket': parse_ticket}
+
+    mode = 'rules'
+    with open(path) as file_handle:
+        for line in file_handle:
+            if line.isspace():
+                continue
+
+            if line == 'your ticket:\n':
+                mode = 'my_ticket'
+                continue
+
+            if line == 'nearby tickets:\n':
+                mode = 'ticket'
+                continue
+
+            modes[mode](line)
+
+    return data
+
+
+def main():
+    ticket_data = parse_file('day16_input.txt')
+    invalid, valid_tickets = find_invalid_values(ticket_data)
+    print(f'Sum of Invalid values = {sum(invalid)}')
+    # 29878
+
+    value_map = define_fields(valid_tickets)
+    print('\nMy ticket:')
+    departure_values = print_ticket(value_map, ticket_data.my_ticket)
+
+    print(f'\nProduct of Departure Values = {prod(departure_values)}')
+    # 855438643439
+
+
+if __name__ == '__main__':
+    main()
